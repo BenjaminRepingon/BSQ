a736aab7e1bae834be8b207cb2125b9a029ccaaa
Larrycovers <espiroux@e2r4p21.42.fr>
2013-09-02 23:14:13 +0200

    Ajout de structure

---
+++ includes/struct.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   struct.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: espiroux <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/09/02 13:01:33 by espiroux          #+#    #+#             */
+/*   Updated: 2013/09/02 13:01:34 by espiroux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef __STRUC_H__
+#	define __STRUC_H__
+
+typedef	struct	st_square
+{
+	int	boolverif;
+	int	x;
+	int	y;
+};
+
+typedef struct st_plateau
+{
+	char	*mem;
+	int	max;
+};
+
+#endif
