c8f78e2881929df899b7c8597aad51569582e982
Emeric SPIROUX <espiroux@e2r4p21.42.fr>
2013-09-03 16:03:12 +0200

    mdification nom plateau -> map et creation main

---
+++ srcs/main.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: espiroux <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/09/02 18:44:43 by espiroux          #+#    #+#             */
+/*   Updated: 2013/09/02 18:44:43 by espiroux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main(int argc, char const *argv[])
+{
+	int i;
+	st_square square;
+	st_plateau plateau;
+
+	i = 1;
+	if (argc == 1)
+		ft_square_copy("");
+	else
+	{
+		while (i < argc)
+		{
+			map = ft_map_copy(argv[i]);
+			i++;
+		}
+		ft_square
+	}
+	return (0);
+}
