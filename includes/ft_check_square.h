01270dfbfd5d1e31d47745aa9da8da9ca373ba8d
Emeric SPIROUX <espiroux@e2r4p21.42.fr>
2013-09-04 23:51:49 +0200

    Diminution ligne map_copy !!!!!!!!!!!!!! Attention bcp de modification
    Test non fini

--- includes/ft_check_square.h
+++ includes/ft_check_square.h
@@ -14,7 +14,7 @@
 # define FT_CHECK_ONE_SQUARE
 #include "struct.h"
 
-int ft_check_one_square(int x, int y, square sq, map mp);
+int	ft_check_one_square(int x, int y, square sq, map mp);
 square	ft_check_square(int size, map mp);
 
 #endif
--- srcs/ft_map_copy.c
+++ srcs/ft_map_copy.c
@@ -16,125 +16,71 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "struct.h"
-#include "ft_error.h"
 #include "function.h"
 
 /*
-**Initialise x and y as i for increment saving of the map    
+**map initialise
 */
 
-void ft_init(int *x, int *y, int *i, int *fd)
+void map_init(map mp)
 {
-	x = 0;
-	y = 0;
-	i = 0;
-	fd = 0;
+	mp.x = 0;
+	mp.y = 0;
+	mp.error = 0;
 }
 
 /*
-**Copy the map in a map's structure
-**find the min between x and y.     
+**count line of the map.
 */
 
-map	ft_map_copy(char *av, int ac)
+void map_count(map mp, char *buf)
 {
-	int	fd;
-	int	ret;
-	int	x;
-	int	y;
-	int	i;
-	int countfind;
+	if (buf[0] == '\n')
+		mp.y++;
+	if (buf[0] != '\n')
+		mp.x++;
+}
+
+/*
+**copie file to struct map in the variable mem.
+*/
+void ft_count_clone_file(map mp, int *fd, char *buf)
+{
 	int count;
+	int i;
+	int countfind;
 	char find_char[13];
-	char	buf[BUFFSIZE + 1];
-	map	mp;
 
 	i = 0;
-	x = 0;
-	y = 0;
-	fd = 0;
-
-
 	count = 0;
 	countfind = 0;
-
-
-
-	mp.error = 0;	
-	mp.mem = (char*)malloc(BUFFSIZE);
-	
-
-	if (ac > 1)
-	{
-		if ((fd = open(av, O_RDONLY)))
-		{
-			mp.error = 1;
-			return (mp);
-		}
-			
-	}
+	map_init(mp);
 	while ((ret = read(fd, buf, BUFFSIZE)))
 	{
 		if (count != 0)
 		{
-			if (buf[0] == '\n')
-				y++;
-			if (buf[0] != '\n')
-				x++;
+			map_count(mp, buf);
 			mp.mem[i] = buf[0];
 			i++;
 		}
-
-
-		/*
-		**sauvegarde de la premiere ligne pour la gestion des caractere.
-		*/
-
-		if (buf[0] == '\n' && count == 0 )
-			count = 1;
-		else if (buf[0] != '\n'&& count == 0)
-		{
-			find_char[countfind] = buf[0];
-			countfind++;
-		}
+		ft_take_char_first_line(&buf, &count, find_char, &countfind);
 		buf[ret] = '\0';
-
-
 	}
-
-	/*
-	**closing file.
-	*/
-
 	if (close(fd))
 		mp.error = 1;
 	mp.mem[i] = '\0';
+	ft_pull_char_first_line(&countfind, map mp, &find_char);
+	ft_square_max( map mp);
+}
 
+/*
+**Send, to the structure give in parameter, 
+**the value biggest value of the size and 
+**x, y max of the tab map.
+*/
 
-	/*
-	**ajout des caractere a la structure.
-	*/
-	if (countfind > 0)
-	{
-		mp.empty = find_char[countfind - 2];
-		mp.obstacle = find_char[countfind - 1];
-		mp.print = find_char[countfind];
-	}
-	else
-		mp.error = 1;
-
-
-
-
-
-	
-	
-
-	
-	/*
-	**Square max.
-	*/
-
+void ft_square_max(int y, int x, map mp)
+{
 	if (y != 0)
 		x = x / y;
 	if (y < x)
@@ -149,7 +95,61 @@ map	ft_map_copy(char *av, int ac)
 		mp.x = x;
 		mp.y = y;
 	}
-	
+}
 
+/*
+**pull all character nessecery for the map in the map's.
+*/
+
+void ft_pull_char_first_line(int *countfind, map mp, char *find_char)
+{
+	if (countfind > 0)
+	{
+		mp.empty = find_char[countfind - 2];
+		mp.obstacle = find_char[countfind - 1];
+		mp.print = find_char[countfind];
+	}
+	else
+		mp.error = 1;
+}
+
+/*
+**Take characters who define the map 
+*/
+
+void ft_take_char_first_line(char *buf, int *count, char *find_char, int *countfind)
+{
+	if (buf[0] == '\n' && count == 0 )
+		count = 1;
+	else if (buf[0] != '\n'&& count == 0)
+	{
+		find_char[countfind] = buf[0];
+		countfind++;
+	}
+}
+
+/*
+**Copy the map in a map's structure
+**find the min between x and y.     
+*/
+
+map	ft_map_copy(char *av, int ac)
+{
+	int	fd;
+	char	buf[BUFFSIZE + 1];
+	map	mp;
+
+
+	fd = 0;
+	mp.mem = (char*)malloc(BUFFSIZE);
+	if (ac > 1)
+	{
+		if ((fd = open(av, O_RDONLY)))
+		{
+			mp.error = 1;
+			return (mp);
+		}	
+	}
+	ft_count_clone_file(mp, &fd, buf);
 	return (mp);
 }
